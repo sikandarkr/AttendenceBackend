9d5c63bf5b53ca2a0daef8e9dcb94358f06d8cfe
sikandarkumar kumar <sikandarkumarkumar@Mac.lan>
2025-06-19 12:00:57 +0530

    cloud watch configured

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/authController');
-
+const logger = require('../utils/logger');
 router.post('/login', controller.login);
 
 router.post('/demo', (req,res)=>{
+    logger.error('Demo route error', { message: err.message, stack: err.stack });
     throw new Error('🔥 Intentional error for testing CloudWatch');
     return res.json({"Data":"Now it's working congrats...."});
 })
