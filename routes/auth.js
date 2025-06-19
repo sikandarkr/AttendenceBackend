ef8ed56ee6d48ba6f64700511d947439f05d43e4
sikandarkumar kumar <sikandarkumarkumar@Mac.lan>
2025-06-19 09:25:04 +0530

    demo api implementation for testing

--- routes/auth.js
+++ routes/auth.js
@@ -4,4 +4,8 @@ const controller = require('../controllers/authController');
 
 router.post('/login', controller.login);
 
+router.post('/demo', (req,res)=>{
+    return res.json({"Data":"Now it's working congrats...."});
+})
+
 module.exports = router;
